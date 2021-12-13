36e7f037be0f853584bdb3f267c166abd7ca87e1
Bakyt <93142140+BahaFalcon@users.noreply.github.com>
2021-12-13 19:49:43 +0600

    Add files via upload

---
+++ soda_test.py
@@ -0,0 +1,16 @@
+class Soda:
+    def __init__(self, ingredient):
+        if isinstance(ingredient, str):
+            self.ingredient = ingredient
+        else:
+            self.ingredient = None
+
+    def show_my_drink(self):
+        if self.ingredient:
+            print(f'Газировка и {self.ingredient}')
+        else:
+            print('Обычная газировка')
+
+soda = Soda('cherry')
+
+soda.show_my_drink()
